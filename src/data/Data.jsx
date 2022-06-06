58e772c793116f44904876942ff82ffcd37bc8b0
Manh-Khuat <101697968+Manh-Khuat@users.noreply.github.com>
2022-06-06 16:26:17 +0700

    Update Data.jsx

--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -10,56 +10,56 @@ export const Data = [
     id: 1,
     title: "iPhone 11 Pro",
     price: 750,
-    desc: "Về tổng thể, iPhone 11 vẫn dùng chung ngôn ngữ thiết kế so với người anh em tiền nhiệm là iPhone Xr. Phía trước vẫn là thiết kế màn hình tai thỏ tràn viền quen thuộc. Tuy nhiên, phiên bản này Apple trau chuốt hơn với mặt lưng được phủ nhám, giảm bám vân tay và tăng trải nghiệm cầm nắm. iPhone 11 128GB cũ sử dụng hiệu quả những đường cong mềm mại xung quanh giúp máy trông mỏng và đẹp hơn trông thấy. Mặt kính cường lực dường như hòa cùng viền nhôm tạo sự đồng nhất, mượt mà, tinh tế mà chỉ những mẫu Flagship siêu cao cấp mới có thể sở hữu.",
+    desc: "iPhone 11 Pro 256GB cho bạn không gian lưu trữ thoải mái, tha hồ chụp ảnh và quay video bằng hệ thống 3 camera sau siêu đỉnh, thời lượng pin dài vô tận cùng sức mạnh khổng lồ từ bộ vi xử lý Apple A13 Bionic.",
     img: "/images/iphone/iphone11pro.jpg"
   },
   {
     id: 2,
     title: "iPhone 11 Pro Max",
     price: 800,
-    desc: "Về tổng thể, iPhone 11 vẫn dùng chung ngôn ngữ thiết kế so với người anh em tiền nhiệm là iPhone Xr. Phía trước vẫn là thiết kế màn hình tai thỏ tràn viền quen thuộc. Tuy nhiên, phiên bản này Apple trau chuốt hơn với mặt lưng được phủ nhám, giảm bám vân tay và tăng trải nghiệm cầm nắm. iPhone 11 128GB cũ sử dụng hiệu quả những đường cong mềm mại xung quanh giúp máy trông mỏng và đẹp hơn trông thấy. Mặt kính cường lực dường như hòa cùng viền nhôm tạo sự đồng nhất, mượt mà, tinh tế mà chỉ những mẫu Flagship siêu cao cấp mới có thể sở hữu.",
+    desc: "iPhone 11 Pro Max sở hữu màn hình đáng tiền, với kích thước 6.5 inch, màn hình Super Retina XDR, mang đến độ sáng ấn tượng, màu sắc cực kỳ chính xác và độ phân giải 2.688 x 1.242 pixel sắc nét.",
     img: "/images/iphone/iphone11promax.png"
   },
   {
     id: 3,
     title: "iPhone 12",
     price: 850,
-    desc: "Về tổng thể, iPhone 11 vẫn dùng chung ngôn ngữ thiết kế so với người anh em tiền nhiệm là iPhone Xr. Phía trước vẫn là thiết kế màn hình tai thỏ tràn viền quen thuộc. Tuy nhiên, phiên bản này Apple trau chuốt hơn với mặt lưng được phủ nhám, giảm bám vân tay và tăng trải nghiệm cầm nắm. iPhone 11 128GB cũ sử dụng hiệu quả những đường cong mềm mại xung quanh giúp máy trông mỏng và đẹp hơn trông thấy. Mặt kính cường lực dường như hòa cùng viền nhôm tạo sự đồng nhất, mượt mà, tinh tế mà chỉ những mẫu Flagship siêu cao cấp mới có thể sở hữu.",
+    desc: "iPhone 12 ra mắt với vai trò mở ra một kỷ nguyên hoàn toàn mới. Tốc độ mạng 5G siêu tốc, bộ vi xử lý A14 Bionic nhanh nhất thế giới smartphone, màn hình OLED tràn cạnh tuyệt đẹp và camera siêu chụp đêm, tất cả đều có mặt trên iPhone 12.",
     img: "/images/iphone/iphone12.png"
   },
   {
     id: 4,
     title: "iPhone 12 Pro",
     price: 888,
-    desc: "Về tổng thể, iPhone 11 vẫn dùng chung ngôn ngữ thiết kế so với người anh em tiền nhiệm là iPhone Xr. Phía trước vẫn là thiết kế màn hình tai thỏ tràn viền quen thuộc. Tuy nhiên, phiên bản này Apple trau chuốt hơn với mặt lưng được phủ nhám, giảm bám vân tay và tăng trải nghiệm cầm nắm. iPhone 11 128GB cũ sử dụng hiệu quả những đường cong mềm mại xung quanh giúp máy trông mỏng và đẹp hơn trông thấy. Mặt kính cường lực dường như hòa cùng viền nhôm tạo sự đồng nhất, mượt mà, tinh tế mà chỉ những mẫu Flagship siêu cao cấp mới có thể sở hữu.",
-    img: "/images/iphone/iphone12promax.png"
+    desc: "iPhone 12 Pro 128GB đi theo thiết kế vuông vức được Apple mang lại từ thế hê iPhone 5 trước đó và điểm thêm một vài chi tiết mới mẻ hiện đại. Phần viền vuông vức tạo ra một cảm giác rất mới mẻ trong thế giới smarthone. Ngoài ra vật liệu thép không gỉ vẫn tương tự như các dòng iPhone cao cấp trước đây cũng tạo một cảm giác cao cấp.",
+    img: "/images/iphone/iphone12pro.png"
   },
   {
     id: 5,
     title: "iPhone 12 Pro Max",
     price: 900,
-    desc: "Về tổng thể, iPhone 11 vẫn dùng chung ngôn ngữ thiết kế so với người anh em tiền nhiệm là iPhone Xr. Phía trước vẫn là thiết kế màn hình tai thỏ tràn viền quen thuộc. Tuy nhiên, phiên bản này Apple trau chuốt hơn với mặt lưng được phủ nhám, giảm bám vân tay và tăng trải nghiệm cầm nắm. iPhone 11 128GB cũ sử dụng hiệu quả những đường cong mềm mại xung quanh giúp máy trông mỏng và đẹp hơn trông thấy. Mặt kính cường lực dường như hòa cùng viền nhôm tạo sự đồng nhất, mượt mà, tinh tế mà chỉ những mẫu Flagship siêu cao cấp mới có thể sở hữu.",
+    desc: "Apple đã trang bị cho điện thoại 12 Pro Max mới này màn hình có kích thước lên đến 6,7 inch, nhờ vậy mà phần viền của máy rất nhỏ. Giống như iPhone 11 Pro Max, lần này Apple cũng dùng màn hình Super Retina XDR. Với độ phân giải 2778 x 1284 pixel, màn hình iPhone 12 Pro Max còn được bo tròn theo thiết kế cong và các góc này nằm trong một hình chữ nhật tiêu chuẩn.",
     img: "/images/iphone/iphone12promax.png"
   },
   {
     id: 6,
     title: "iPhone 13",
     price: 920,
-    desc: "Về tổng thể, iPhone 11 vẫn dùng chung ngôn ngữ thiết kế so với người anh em tiền nhiệm là iPhone Xr. Phía trước vẫn là thiết kế màn hình tai thỏ tràn viền quen thuộc. Tuy nhiên, phiên bản này Apple trau chuốt hơn với mặt lưng được phủ nhám, giảm bám vân tay và tăng trải nghiệm cầm nắm. iPhone 11 128GB cũ sử dụng hiệu quả những đường cong mềm mại xung quanh giúp máy trông mỏng và đẹp hơn trông thấy. Mặt kính cường lực dường như hòa cùng viền nhôm tạo sự đồng nhất, mượt mà, tinh tế mà chỉ những mẫu Flagship siêu cao cấp mới có thể sở hữu.",
+    desc: "Đánh giá iPhone 13 128GB cho thấy ngoại hình máy không có nhiều khác biệt so với iPhone 12. Apple tiếp tục sử dụng cạnh viền vát phẳng mang đến sự nam tính và mạnh mẽ. Phần thân máy siêu mỏng, kết hợp với mặt kính cường lực bóng bẩy và khung nhôm chắc chắn mang đến độ bền tốt hơn.",
     img: "/images/iphone/iphone13.jpg"
   },
   {
     id: 7,
     title: "iPhone 13 Pro",
     price: 980,
-    desc: "Về tổng thể, iPhone 11 vẫn dùng chung ngôn ngữ thiết kế so với người anh em tiền nhiệm là iPhone Xr. Phía trước vẫn là thiết kế màn hình tai thỏ tràn viền quen thuộc. Tuy nhiên, phiên bản này Apple trau chuốt hơn với mặt lưng được phủ nhám, giảm bám vân tay và tăng trải nghiệm cầm nắm. iPhone 11 128GB cũ sử dụng hiệu quả những đường cong mềm mại xung quanh giúp máy trông mỏng và đẹp hơn trông thấy. Mặt kính cường lực dường như hòa cùng viền nhôm tạo sự đồng nhất, mượt mà, tinh tế mà chỉ những mẫu Flagship siêu cao cấp mới có thể sở hữu.",
-    img: "/images/iphone/iphone13promax.jpg"
+    desc: "Thiết kế của iPhone 13 Pro không khác những tin đồn rò rỉ trước đó. Máy có khung viền vuông vức tương tự iPhone 12 nhưng tinh tế hơn. Được hoàn thiện từ thép không gỉ và mặt kính Cinematic Shield cứng cáp bảo vệ máy khỏi trầy xước, bụi bẩn hay chất lỏng chuẩn IP68.Dòng iPhone cao cấp năm nay sở hữu 4 tùy chọn màu sắc bao gồm Bạc (Silver), Than chì (Graphite), Vàng (Gold) và màu Xanh dương nhạt (Sierra Blue) hoàn toàn mới lạ",
+    img: "/images/iphone/iphone13pro.png"
   },
   {
     id: 8,
     title: "iPhone 13 Pro Max",
     price: 1000,
-    desc: "Về tổng thể, iPhone 11 vẫn dùng chung ngôn ngữ thiết kế so với người anh em tiền nhiệm là iPhone Xr. Phía trước vẫn là thiết kế màn hình tai thỏ tràn viền quen thuộc. Tuy nhiên, phiên bản này Apple trau chuốt hơn với mặt lưng được phủ nhám, giảm bám vân tay và tăng trải nghiệm cầm nắm. iPhone 11 128GB cũ sử dụng hiệu quả những đường cong mềm mại xung quanh giúp máy trông mỏng và đẹp hơn trông thấy. Mặt kính cường lực dường như hòa cùng viền nhôm tạo sự đồng nhất, mượt mà, tinh tế mà chỉ những mẫu Flagship siêu cao cấp mới có thể sở hữu.",
+    desc: "iPhone 13 Pro Max vẫn giữ ngôn ngữ thiết kế ‘hoài cổ’ với các cạnh viền vuông vức như iPhone 12 series. Sự thay đổi lớn nhất ở vẻ ngoài năm na chính là phần cụm camera: Cụm camera sau lồi, to, sát viền hơn và được bảo vệ bởi một lớp kính. Đồng thời, sau bao năm chờ đợi phần tai thỏ trên màn hình cuối cùng cũng được thu nhỏ.",
     img: "/images/iphone/iphone13promax.jpg"
   },
 ];
